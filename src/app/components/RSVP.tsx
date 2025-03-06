e874a4d18c2eafd8fe9dbc9fb0b4e08b34adc1d9
Sofia Martin <sofiamartinux@gmail.com>
2025-03-05 22:44:06 -0800

    working form, style improvements

--- src/app/components/RSVP.tsx
+++ src/app/components/RSVP.tsx
@@ -1,13 +1,25 @@
 "use client";
-import React, { useState, useRef } from "react";
+import React, { useRef } from "react";
 import Image from "next/image";
+import emailjs from "@emailjs/browser";
 
 const RSVP = () => {
-  const formRef = useRef(null);
-  const [done, resetButton] = useState(false);
-  const handleButton = () => {
-    resetButton(!done);
-    console.log(done);
+  const form = useRef(null!);
+  const sendEmail = (e: Event) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm("service_3659z7o", "template_1h2plqr", form.current, {
+        publicKey: "AcBW8zD6h1t_bveJH",
+      })
+      .then(
+        () => {
+          console.log("SUCCESS!");
+        },
+        (error) => {
+          console.log("FAILED...", error.text);
+        }
+      );
   };
 
   return (
@@ -15,11 +27,11 @@ const RSVP = () => {
       <div className="m-auto mb-4 w-full max-w-[1240px] px-2">
         <div className="gap-8 md:grid lg:grid-cols-5">
           {/* left */}
-          <div className="co-span-3 h-full w-full lg:col-span-2 lg:px-0">
+          <div className="col-span-3 h-full w-full lg:col-span-2 lg:px-0">
             <div className="h-full">
               <div className="text-center md:text-left">
                 <Image
-                  className="rounded-xl mx-auto my-2"
+                  className="rounded-xl mx-auto mb-8 md:my-2"
                   src="/watercolor.jpg"
                   alt="Watercolor depiction of Penelope and Sofia SCUBA Diving in Hawaii"
                   width={375}
@@ -33,9 +45,9 @@ const RSVP = () => {
           <div className="col-span-3 h-auto w-full">
             <div className="">
               <form
-                action="https://getform.io/f/cec7a2a2-5959-47a2-adea-d1dcc55300f9"
-                method="POST"
-                ref={formRef}
+                ref={form}
+                onSubmit={sendEmail}
+                className="flex flex-col gap-4"
                 autoComplete="on"
               >
                 <div className="grid w-full gap-4 md:grid-cols-2">
@@ -45,7 +57,7 @@ const RSVP = () => {
                     </label>
                     <input
                       type="text"
-                      className="rounded-md border border-gray-500 p-3"
+                      className="rounded-md border border-gray-500 p-3 dark:bg-transparent"
                       name="name"
                       id="name"
                       required
@@ -59,7 +71,7 @@ const RSVP = () => {
                     </label>
                     <input
                       type="email"
-                      className="rounded-md border border-gray-500 p-3"
+                      className="rounded-md border border-gray-500 p-3 dark:bg-transparent"
                       name="email"
                       id="email"
                       required
@@ -68,36 +80,66 @@ const RSVP = () => {
                     />
                   </div>
                 </div>
-                <div className="flex flex-col">
-                  <fieldset>
-                    <legend className="py-2 text-sm">
-                      Attending <span className="text-red"> *</span>
-                    </legend>
-                    <div className="flex items-center gap-1">
-                      <input
-                        type="radio"
-                        className="rounded-md border border-gray-500 p-3"
-                        name="attendance"
-                        id="yes"
-                        defaultChecked
-                      />
-                      <label htmlFor="yes">Yes</label>
-                    </div>
-                    <div className="flex items-center gap-1">
-                      <input type="radio" name="attendance" id="no" />
-                      <label htmlFor="no">No</label>
-                    </div>
-                  </fieldset>
+                <div className="grid w-full gap-4 md:grid-cols-2">
+                  <div className="flex flex-col">
+                    <fieldset>
+                      <legend className="py-2 text-sm">
+                        Attending <span className="text-red"> *</span>
+                      </legend>
+                      <div className="flex items-center h-[50px] border border-blue-600 rounded-md overflow-hidden focus-within:outline">
+                        <label htmlFor="yes" className="cursor-pointer w-full">
+                          <input
+                            type="radio"
+                            className="sr-only peer"
+                            name="attendance"
+                            id="yes"
+                            value="yasssss"
+                            defaultChecked
+                          />
+                          <span className="p-4 flex justify-center peer-checked:bg-blue-600">
+                            Yes
+                          </span>
+                        </label>
+                        <label htmlFor="no" className="cursor-pointer w-full">
+                          <input
+                            type="radio"
+                            name="attendance"
+                            id="no"
+                            value="nooo :("
+                            className="sr-only peer"
+                          />
+                          <span className="p-4 flex justify-center peer-checked:bg-blue-600">
+                            No
+                          </span>
+                        </label>
+                      </div>
+                    </fieldset>
+                  </div>
+                  <div className="flex flex-col">
+                    <label className="py-2 text-sm" htmlFor="kids">
+                      Kids?
+                    </label>
+                    <input
+                      type="text"
+                      className="rounded-md border border-gray-500 p-3 dark:bg-transparent"
+                      name="kids"
+                      id="kids"
+                      autoComplete="Full name"
+                    />
+                  </div>
                 </div>
                 <div className="flex flex-col">
-                  <label className="py-2 text-sm" htmlFor="subject">
+                  <label
+                    className="py-2 text-sm"
+                    htmlFor="dietary_restrictions"
+                  >
                     Dietary Restrictions
                   </label>
                   <input
                     type="text"
-                    className="rounded-md border border-gray-500 p-3"
-                    name="subject"
-                    id="subject"
+                    className="rounded-md border border-gray-500 p-3 dark:bg-transparent"
+                    name="dietary_restrictions"
+                    id="dierary_restrictions"
                   />
                 </div>
                 <div className="flex flex-col">
@@ -106,19 +148,18 @@ const RSVP = () => {
                   </label>
                   <textarea
                     rows={4}
-                    className="rounded-md border border-gray-500 p-3"
+                    className="rounded-md border border-gray-500 p-3 dark:bg-transparent focus:outline-solid"
                     name="message"
                     id="message"
                   />
                 </div>
                 <input type="hidden" name="_gotcha" className="hidden"></input>
                 <div className="flex flex-col">
-                  <button
-                    onClick={handleButton}
-                    className="mx-auto mt-8 p-4 transition duration-300 ease-in-out hover:shadow-xl"
-                  >
-                    Send Message
-                  </button>
+                  <input
+                    type="submit"
+                    value="Send"
+                    className="bg-blue-600 rounded-md mt-2 py-2 px-16 md:w-fit mx-auto cursor-pointer hover:shadow-sm hover:bg-blue-500 hover:scale-95"
+                  />
                 </div>
               </form>
             </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,16 +2,18 @@ import Image from "next/image";
 import RSVP from "./components/RSVP";
 import guanajuato from "../../public/kiss.jpg";
 import birchSunset from "../../public/sunset_view_from_venue.jpg";
+import { GiBigDiamondRing } from "react-icons/gi";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div>
-      <div className="fixed w-full backdrop-blur">
-        <div className="flex p-5">
-          <div className="basis-1/4 font-[family-name:var(--font-pacifico)]">
+      <div className="fixed w-full flex flex-col top-0 backdrop-blur">
+        <div className="container mx-auto flex h-16 p-5">
+          <div className="md:basis-1/4 font-[family-name:var(--font-pacifico)]">
             Penelope & Sofia&apos;s Wedding
           </div>
-          <nav className="basis-3/4">
+          <nav className="hidden md:flex basis-3/4 justify-end">
             <ul className="list-none items-center md:flex justify-end gap-5">
               <li>Wedding Details</li>
               <li>Accomodations</li>
@@ -21,15 +23,18 @@ export default function Home() {
           </nav>
         </div>
       </div>
-      <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen pb-20 gap-16 font-[family-name:var(--font-geist-sans)]">
-        <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-          <section className="min-h-screen w-full">
+      <div className="pb-20 pt-16 font-[family-name:var(--font-geist-sans)]">
+        <main className="flex flex-col gap-20 items-center sm:items-start">
+          <section className="w-full">
             <div className="md:grid grid-cols-2">
               <div className="flex flex-col w-full items-center justify-center">
                 <h1 className="text-center">
-                  Penelope and Sofia are tying the knot
+                  Penelope and Sofia{" "}
+                  <span className="text-nowrap">are tying the knot</span>
                 </h1>
-                <p className="p-4 text-center">Join us in celebration</p>
+                <p className="pb-3 md:p-4 text-center font-[family-name:var(--font-pacifico)]">
+                  Join us in celebration
+                </p>
               </div>
               <Image
                 className="mx-auto w-2/3"
@@ -39,15 +44,15 @@ export default function Home() {
               />
             </div>
           </section>
-          <section className="container p-2 text-center mx-auto min-h-screen">
-            <h1>RSVP</h1>
+          <section className="container mx-auto text-center py-8">
+            <h1 className="text-4xl md:text-5xl">RSVP</h1>
             <RSVP />
           </section>
-          <section className="container mx-auto p-2 text-center min-h-screen">
+          <section className="container mx-auto text-center py-8">
             <h1>Venue Information</h1>
-            <p>Birch Aquarium</p>
-            <p>2300 Expedition Way, La Jolla, CA 92037</p>
-            <p>
+            <p className="p-0">Birch Aquarium</p>
+            <p className="p-0">2300 Expedition Way, La Jolla, CA 92037</p>
+            <p className="max-w-3xl mx-auto mt-4">
               Birch Aquarium is a public aquarium in La Jolla, a community of
               San Diego, California. It serves as the public outreach center for
               Scripps Institution of Oceanography at the University of
@@ -55,53 +60,55 @@ export default function Home() {
               the aquarium each year.
             </p>
             <Image
-              className="mx-auto w-1/2"
+              className="mx-auto md:w-1/2 mt-6"
               src={birchSunset}
               alt="Photo of Penelope and Sofia on vacation in Guanajuato, MX"
               priority
             />
           </section>
-          <section className="container mx-auto p-2 text-center min-h-screen flex flex-col gap-8">
+          <section className="container mx-auto py-8 text-center">
             <h1>Itinerary</h1>
-            <div>
-              <h2>Thursday Night Dinner</h2>
-              <p>Thursday, July 10 @ 7pm</p>
-              <p>Ballast Point Brewing Company</p>
-              <p>9045 Carroll Way</p>
-            </div>
-            <div>
-              <h2>Wedding</h2>
-              <p>Friday, July 11</p>
-              <p>Cocktail Hour @ 6pm</p>
-              <p>Ceremony @ 7pm</p>
-              <p>Dinner @ 8pm</p>
-              <p>Last Call @ 10pm</p>
-              <p>Ends @ 11pm</p>
-            </div>
-            <div>
-              <h2>After Party</h2>
-              <p>Friday, Jully 11 @ 11pm</p>
-              <p>Location TBD</p>
-            </div>
-            <div>
-              <h2>Saturday Brunch</h2>
-              <p>Saturday, July 12 @ 12pm</p>
-              <p>Location TBD</p>
-            </div>
-            <div>
-              <h2>Sunday Beach Day?</h2>
-              <p>TBD</p>
+            <div className="flex flex-col gap-8">
+              <div>
+                <h2>Thursday Night Dinner</h2>
+                <p>Thursday, July 10 @ 7pm</p>
+                <p>Ballast Point Brewing Company</p>
+                <p>9045 Carroll Way</p>
+              </div>
+              <div>
+                <h2>Wedding</h2>
+                <p>Friday, July 11</p>
+                <p>Cocktail Hour @ 6pm</p>
+                <p>Ceremony @ 7pm</p>
+                <p>Dinner @ 8pm</p>
+                <p>Last Call @ 10pm</p>
+                <p>Ends @ 11pm</p>
+              </div>
+              <div>
+                <h2>After Party</h2>
+                <p>Friday, Jully 11 @ 11pm</p>
+                <p>Location TBD</p>
+              </div>
+              <div>
+                <h2>Saturday Brunch</h2>
+                <p>Saturday, July 12 @ 12pm</p>
+                <p>Location TBD</p>
+              </div>
+              <div>
+                <h2>Sunday Beach Day?</h2>
+                <p>TBD</p>
+              </div>
             </div>
           </section>
-          <section className="container mx-auto p-2 min-h-screen">
+          <section className="container text-center mx-auto p-8">
             <h1>Where to stay</h1>
-            <p>
+            <p className="max-w-3xl mx-auto">
               We&apos;ve reserved rooms for our out-of-town guests at the
               following hotels. Mention the Oseguera-Martin wedding when booking
               your room.
             </p>
           </section>
-          <section className="container mx-auto p-2 text-center min-h-screen">
+          <section className="container mx-auto text-center p-4">
             <h1>Things to do</h1>
             <ul>
               <li>San Diego Zoo</li>
@@ -109,25 +116,23 @@ export default function Home() {
               <li>UTC Mall</li>
             </ul>
           </section>
-          <section className="container mx-auto p-2 min-h-screen">
+          <section className="container text-center mx-auto p-8">
             <h1>Registry</h1>
-            <p>
+            <p className="max-w-3xl mx-auto">
               Your presence is the greatest &quot;irreplaceable&quot; gift, but
               if you&apos;d like to &quot;upgrade us&quot; here&apos;s where you
               can find some of the &quot;best things we never had&quot; :
             </p>
           </section>
         </main>
-        <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-          <p className="flex items-center gap-2 hover:underline hover:underline-offset-4">
-            English/Spanish
-          </p>
-          <p className="flex items-center gap-2 hover:underline hover:underline-offset-4">
-            Light/Dark
-          </p>
-          <p className="flex items-center gap-2 hover:underline hover:underline-offset-4">
-            Back to top{" "}
-          </p>
+        <footer className="flex items-center justify-center">
+          <Link
+            href="/"
+            className="cursor-pointer hover:underline hover:underline-offset-4"
+          >
+            Back to top &nbsp;
+          </Link>
+          <GiBigDiamondRing />
         </footer>
       </div>
     </div>
